07d69dcab321bd546ac93a12d048212b2946ec3c
Ziatical <freyasemerson@gmail.com>
2024-09-17 14:03:49 -0400

    First half done. JSON stuff done, waiting on professor.

---
+++ src/server.js
@@ -0,0 +1,37 @@
+// Requires
+const http = require('http');
+const htmlHandler = require('./htmlResponses.js');
+const jsonHandler = require('./jsonResponses.js');
+
+// Port
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+// URL Structure
+const urlStruct = {
+  '/': htmlHandler.getIndex,
+  '/style.css': htmlHandler.getCSS,
+  '/success': jsonHandler.success,
+  '/badRequest': jsonHandler.badRequest,
+  '/unauthorized': jsonHandler.unauthorized,
+  '/forbidden': jsonHandler.forbidden,
+  '/internal': jsonHandler.internal,
+  '/notImplemented': jsonHandler.notImplemented,
+  notFound: jsonHandler.notFound,
+};
+
+// Handle Requests
+const onRequest = (request, response) => {
+  const protocol = request.connection.encrypted ? 'https' : 'http';
+  const parsedUrl = new URL(request.url, `${protocol}://${request.headers.host}`);
+
+  if (urlStruct[parsedUrl.pathname]) {
+    return urlStruct[parsedUrl.pathname](request, response);
+  }
+
+  return urlStruct.notFound(request, response);
+};
+
+// Start Server
+http.createServer(onRequest).listen(port, () => {
+  console.log(`Listening on 127.0.0.1: ${port}`);
+});
